84f43827b1cea8dd5d952743adf26fbeb1d21df2
YuvalAmrami <yuvalamrami2444@gmail.com>
2024-01-08 18:03:52 +0200

    setting up the API

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ WORKDIR /app
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 COPY . /app
-CMD ["python", "app.py"]
+EXPOSE 3000
+CMD ["uvicorn", "app:app", "--reload", "--port" ,"3000" ,"--host" ,"0.0.0.0"]
+# CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import os
+
 from docx import Document
 
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain.llms import OpenAI
 
 # for costs calculations
 # from langchain.callbacks import get_openai_callback
 
-
-# OPENAI_API_KEY = os.environ['OPENAI_KEY']
+############################################# setting up the AI:
+OPENAI_API_KEY = "aaaa"
+#  os.environ['OPENAI_KEY']
 fileName = "PythonExercise.docx" 
 
-
+###################### AI functions:
 # read the text
 def fileReader(fileName):
     doc = Document(fileName)
@@ -41,18 +46,46 @@ def translateDataToKnowledge(chunksOfText):
       return FAISS.from_texts(chunksOfText, embeddings)
 
 def getOpenAiAnswer(knowledge, question, chain):
-    # semantic search in the knowledge base
-    docs = knowledge.similarity_search(question)
-    # GPT answer
-    return chain.run(input_documents=docs, question=question)
+
+    print(question)
+    return "for now"
+
+    # # semantic search in the knowledge base
+    # docs = knowledge.similarity_search(question)
+    # # GPT answer
+    # return chain.run(input_documents=docs, question=question)
+
+# loading and setting the file:
+# text, paragraphs = fileReader(fileName)
+# cntxText = textSpliter(text)
+# knowledge = translateDataToKnowledge(cntxText)
+knowledge = "Fff"
+
+# setting the AI system
+# llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+# chain = load_qa_chain(llm, chain_type="stuff")
+chain = "aa"
 
 
-text, paragraphs = fileReader(fileName)
-cntxText = textSpliter(text)
-knowledge = translateDataToKnowledge(cntxText)
+############################# setting the API 
 
-llm = OpenAI(openai_api_key=OPENAI_API_KEY)
-chain = load_qa_chain(llm, chain_type="stuff")
+app = FastAPI()
+
+class Question(BaseModel):
+    question: str
 
 
-answer = getOpenAiAnswer(knowledge, question, chain)
+@app.get("/")
+def hello():
+    return "hello"
+
+@app.post("/")
+def askQuestion(TheQuestion: Question):
+    # print(TheQuestion.question)
+    # return getOpenAiAnswer(knowledge, question, chain)
+
+
+# uvicorn.run(app,  port= int(os.environ['API_PORT']))
+
+# host=os.environ['API_HOST'] ,
+

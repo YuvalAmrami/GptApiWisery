0b1832967b08499371de1b4bf64f711fafd3760d
YuvalAmrami <yuvalamrami2444@gmail.com>
2024-01-08 22:04:29 +0200

    cleaning the code

--- app.py
+++ app.py
@@ -8,14 +8,8 @@ from pydantic import BaseModel
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-
 from langchain.chains.question_answering import load_qa_chain
-# from langchain.chains.combine_documents import create_stuff_documents_chain
-
-# from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-
-
 
 # for costs calculations
 # from langchain.callbacks import get_openai_callback
@@ -49,8 +43,6 @@ def translateDataToKnowledge(chunksOfText):
       return FAISS.from_texts(chunksOfText, embeddings)
 
 def getOpenAiAnswer(knowledge, question, chain):
-
-    # print(question)
     # semantic search in the knowledge base
     docs = knowledge.similarity_search(question)
     # GPT answer
@@ -83,7 +75,6 @@ def hello():
 
 @app.post("/")
 def askQuestion(TheQuestion: Question):
-    # print(TheQuestion.question)
     question = TheQuestion.question
     return getOpenAiAnswer(knowledge, question, chain)
 
